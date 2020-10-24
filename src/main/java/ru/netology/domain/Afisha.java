9ed862ee69c6da409b8b3955f64e33a77a04b7f1
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-10-24 11:42:01 +0300

    initial

---
+++ src/main/java/ru/netology/domain/Afisha.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+public class Afisha {
+    public class PurchaseItem {
+        private int id;
+        private int filmId;
+        private String filmName;
+        private int filmPrice;
+        private int count;
+    }
+}
