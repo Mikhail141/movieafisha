c8fa1d8b5bbd0b535cedd6b1edd9eaa2756a8c99
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-10-26 17:49:56 +0300

    change

--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -5,37 +5,21 @@ import ru.netology.domain.AfishaItem;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AfishaManagerTest {
+    private AfishaItem[]films = new AfishaItem[0];
     @Test
     public void shouldAddFilm(){
         AfishaManager manager = new AfishaManager();
-        int idToAdd = 1;
+
+
+
         AfishaItem f = new AfishaItem(1,1,"f",1,1);
-        AfishaItem s = new AfishaItem(2,2,"s",1,1);
-        AfishaItem th = new AfishaItem(3,3,"th",1,1);
-        AfishaItem fo = new AfishaItem(4,4,"fo",1,1);
-        AfishaItem fi = new AfishaItem(5,5,"fi",1,1);
-        AfishaItem si = new AfishaItem(6,6,"si",1,1);
-        AfishaItem se = new AfishaItem(7,7,"se",1,1);
-        AfishaItem et = new AfishaItem(8,8,"et",1,1);
-        AfishaItem n = new AfishaItem(9,9,"n",1,1);
-        AfishaItem t = new AfishaItem(10,10,"t",1,1);
 
         manager.add(f);
-        manager.add(s);
-        manager.add(th);
-        manager.add(fo);
-        manager.add(fi);
-        manager.add(si);
-        manager.add(se);
-        manager.add(et);
-        manager.add(n);
-        manager.add(t);
 
 
 
-
+        AfishaItem[] expected = new AfishaItem[] {f};
         AfishaItem[] actual = manager.getAll();
-        AfishaItem[] expected = new AfishaItem[] {f,s,th,fo,fi,si,se,et,n,t};
         assertArrayEquals(expected,actual);
 
 
