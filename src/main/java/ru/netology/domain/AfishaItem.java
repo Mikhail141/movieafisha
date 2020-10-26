8b13ba4c287170dee3d33ff8896a58ed73b9f338
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-10-26 17:01:32 +0300

    add

--- src/main/java/ru/netology/domain/AfishaItem.java
+++ src/main/java/ru/netology/domain/AfishaItem.java
@@ -8,15 +8,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 
-    public class AfishaItem {
-        private int id;
-        private int filmId;
-        private String filmName;
-        private int filmPrice;
-        private int count;
+public class AfishaItem {
+    private int id;
+    private int filmId;
+    private String filmName;
+    private int filmPrice;
+    private int count;
+
+    private AfishaItem film1;
+    private AfishaItem film2;
+    private AfishaItem film3;
+    private AfishaItem film4;
+    private AfishaItem film5;
+    private AfishaItem film6;
+    private AfishaItem film7;
+    private AfishaItem film8;
+    private AfishaItem film9;
+    private AfishaItem film10;
 
 
-    }
+
+}
 
 
 
--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -9,11 +9,33 @@ public class AfishaManagerTest {
     public void shouldAddFilm(){
         AfishaManager manager = new AfishaManager();
         int idToAdd = 1;
-        AfishaItem first = new AfishaItem(1,1,"first",1,1);
-        manager.add(first);
+        AfishaItem f = new AfishaItem(1,1,"f",1,1);
+        AfishaItem s = new AfishaItem(2,2,"s",1,1);
+        AfishaItem th = new AfishaItem(3,3,"th",1,1);
+        AfishaItem fo = new AfishaItem(4,4,"fo",1,1);
+        AfishaItem fi = new AfishaItem(5,5,"fi",1,1);
+        AfishaItem si = new AfishaItem(6,6,"si",1,1);
+        AfishaItem se = new AfishaItem(7,7,"se",1,1);
+        AfishaItem et = new AfishaItem(8,8,"et",1,1);
+        AfishaItem n = new AfishaItem(9,9,"n",1,1);
+        AfishaItem t = new AfishaItem(10,10,"t",1,1);
+
+        manager.add(f);
+        manager.add(s);
+        manager.add(th);
+        manager.add(fo);
+        manager.add(fi);
+        manager.add(si);
+        manager.add(se);
+        manager.add(et);
+        manager.add(n);
+        manager.add(t);
+
+        manager.add(f);
+
 
         AfishaItem[] actual = manager.getAll();
-        AfishaItem[] expected = new AfishaItem[] {first};
+        AfishaItem[] expected = new AfishaItem[] {f};
         assertArrayEquals(expected,actual);
 
 
